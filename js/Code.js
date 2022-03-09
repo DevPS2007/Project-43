bea5b2a3dc0deab7b6012db43002eed970a342b3
DevPS2007 <77735817+DevPS2007@users.noreply.github.com>
2022-03-09 15:33:49 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "TOM MARVOLO RIDDLE";
+const accessCode2 = "SEVEN";
+const accessCode3 = "ALBUS DUMBLEDORE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("M R T O E O A M D R V I D L O L", 100,50)
+    fill("lightblue")
+    text("Hint:Lord Voldemorts real name!!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("E E S N V", 700,150)
+    fill("lightblue")
+    text("Hint:Number of horcrux made by Lord Voldemort!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A O S M U L R D E D E U B L B", 100,250)
+    fill("lightblue")
+    text("Hint:Name the wizard that Lord Voldemort feared!!", 100,270)
+
+}
